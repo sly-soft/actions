ad8d03562deaecdbdc738f70b163476800160998
Jeff Sylvester <sylvesterjj@gmail.com>
2024-04-04 14:35:18 -0400

    adds more structure

--- .github/scripts/update-outside-collaborators.ps1
+++ .github/scripts/update-outside-collaborators.ps1
@@ -1,13 +1,31 @@
 Write-Host "**update-outside-collaborators**"
 
 $org = $env:OUTSIDE_COLLABORATORS_GITHUB_ORG
+#$token = $env:GITHUB_TOKEN
+
+function LoadCollaborators($repo) {
+    Write-Host "Loading collaborators from file '$repo'"
+    
+    $collaborators = New-Object Collections.Generic.List[string]
+    foreach ($collaborator in $content) {
+        $collaborators.Add($collaborator)
+    }
+    return $collaborators
+}
+
+function HandleRepo($repo) {
+    Write-Host "Handling Repo '$org/$repo'"
+
+    $collaborators = LoadCollaborators $repo
+
+    Write-Host $collaborators
+}
+
+
+#main logic
 
 Set-Location ./external-collaborators
 
 foreach ($file in Get-ChildItem) {
-    Write-Host "Repo: $org/$($file.name)"
-    $content = Get-Content $file.Name
-    foreach ($line in $content) {
-        Write-Host "  External Collaborator:" $content
-    }
+    HandleRepo $file.name
 }
