dcf0171422033258efc54a79c7294160def94f3a
Jeff Sylvester <sylvesterjj@gmail.com>
2024-04-04 13:46:55 -0400

    adds external-collaborators directory and test file

--- .github/scripts/update-outside-collaborators.ps1
+++ .github/scripts/update-outside-collaborators.ps1
@@ -1,1 +1,6 @@
-Write-Host "Org: $env:OUTSIDE_COLLABORATORS_GITHUB_ORG"
+# Write-Host "Org: $env:OUTSIDE_COLLABORATORS_GITHUB_ORG"
+Write-Host "**update-outside-collaborators"
+
+Set-Location external-collaborators
+
+Write-Host ls
