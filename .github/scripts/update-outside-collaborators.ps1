f34e154a4f91659dab531e4f5d60919d59dd94cd
Jeff Sylvester <sylvesterjj@gmail.com>
2024-04-04 13:48:21 -0400

    changes ls to dir because this is powershell...

--- .github/scripts/update-outside-collaborators.ps1
+++ .github/scripts/update-outside-collaborators.ps1
@@ -1,6 +1,6 @@
 # Write-Host "Org: $env:OUTSIDE_COLLABORATORS_GITHUB_ORG"
-Write-Host "**update-outside-collaborators"
+Write-Host "**update-outside-collaborators**"
 
 Set-Location external-collaborators
 
-Write-Host ls
+Write-Host dir
